a5026eb655da04048f7553443a4efbc40f08ebb1
Hugo <hugo.carregno@gmail.com>
2019-07-02 12:13:32 -0300

    rama de prueba

---
+++ hugo/config/global.php
@@ -0,0 +1,7 @@
+<?php
+define("CONTROLADOR_DEFECTO", "Usuario");
+define("ACCION_DEFECTO", "index");
+define("NOMBRE_EMPRESA","Mi empresa");
+//session_start();
+//Otros parámetros generales de la aplicación
+?>
